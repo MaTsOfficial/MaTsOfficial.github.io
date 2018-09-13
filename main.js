744b75c53c4666e4950bcf3009cf4e0de050ea30
MaTsOfficial <41835321+MaTsOfficial@users.noreply.github.com>
2018-09-13 20:38:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,16 +3,32 @@ var parent = document.getElementsByClassName("addedContent")[0];
 var amount = 0;
 var data = document.getElementById("data").value;
 var type = 0;
+var doccolor = document.getElementById("color").value;
+var textmargin = document.getElementsByName("textmargin");
+var textWeight = document.getElementsByName("textWeight");
 
 function add() {
     data = document.getElementById("data").value;
+    doccolor = document.getElementById("color").value;
+    textmargin = document.getElementsByName("textmargin");
+    textWeight = document.getElementsByName("textWeight");
     if (data != "") {
         amount += 1;
         addedElements[amount] = document.createElement("div")
         parent.appendChild(addedElements[amount]);
         if (type === 0) {addedElements[amount].innerHTML = "<li>"+data+"</li>";}
-        if (type === 1) {addedElements[amount].innerHTML = "<h1>"+data+"</h1>";}
+        if (type === 1) {
+            addedElements[amount].innerHTML = "<h1>"+data+"</h1>";
+            addedElements[amount].style.color = doccolor;
+            addedElements[amount].style.borderLeft = "10px solid "+doccolor;
+        }
         if (type === 2) {addedElements[amount].innerHTML = "<p>"+data+"</p>";}
+        if (textmargin[0].checked) {addedElements[amount].style.textAlign = "left";}
+        if (textmargin[1].checked) {addedElements[amount].style.textAlign = "center";}
+        if (textmargin[2].checked) {addedElements[amount].style.textAlign = "right";}
+        if (textWeight[0].checked) {addedElements[amount].style.fontWeight = "bold";}
+        if (textWeight[1].checked) {addedElements[amount].style.fontStyle = "italic";}
+        if (textWeight[2].checked) {addedElements[amount].style.textDecoration = "underline";}
         addedElements[amount].addEventListener("click", function () {parent.removeChild(this);}, false)
         //data = document.getElementById("data").value = "";
     }
